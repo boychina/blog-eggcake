fcc8fcb1285e5ca53fd08c534b05979ee2007186
boychina <boychina@outlook.com>
2020-11-22 00:44:01 +0800

    feat: add heightlight.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
-import '../styles/index.css'
+import '../styles/index.css';
+// 主题参考 https://highlightjs.org/static/demo/
+// import 'highlight.js/styles/atom-one-dark.css';
+import 'highlight.js/styles/night-owl.css';
+// import 'highlight.js/styles/vs2015.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
