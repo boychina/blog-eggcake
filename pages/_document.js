d6fbae9583741ff4796ca93b64a80f9bcc68d784
boychina <boychina@outlook.com>
2020-12-31 17:00:35 +0800

    feat: add baidu statistics

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,23 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <script type="text/javascript">
+            {
+              `
+                var _hmt = _hmt || [];
+                (function() {
+                  var hm = document.createElement('script');
+                  hm.src = 'https://hm.baidu.com/hm.js?984a14f3e0e615ff993c5631e3c3e660';
+                  var s = document.getElementsByTagName('script')[0];
+                  s.parentNode.insertBefore(hm, s);
+                })();
+              `
+            }
+          </script>
+        </Head>
         <body>
-          <Main/>
+          <Main />
           <NextScript />
         </body>
       </Html>
