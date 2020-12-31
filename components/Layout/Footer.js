1d736989d03a2ba42205e84758b3a5131e954818
boychina <boychina@outlook.com>
2020-12-31 16:24:18 +0800

    feat: add record

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -10,15 +10,19 @@ export default function Footer() {
             <a
               href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
               className="text-sm mx-3 hover:underline"
-            >
-              <div className="flex items-center"><GithubOutlined className="mr-1"/> GitHub</div>
+              >
+              <div className="flex items-center"><GithubOutlined className="mr-1" /> GitHub</div>
             </a>
             <span>&copy;2021 boychina</span>
           </div>
         </div>
-        <div className="text-sm text-center mt-2 pb-8 text-gray-500">
+        <div className="text-sm mt-2 pb-8 text-gray-500 flex justify-center">
           <a href="http://beian.miit.gov.cn" target="_blank">
             ICP备案号：蜀ICP备2020034346号-1
+          </a>
+          <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51010802001016" className="ml-2 flex" >
+            <img src="/assets/备案图标.png" />
+            川公网安备 51010802001016号
           </a>
         </div>
       </div>
