4638f38e9c7336ef530b491d9cc0bdd097da0dd3
boychina <boychina@outlook.com>
2021-01-02 21:32:30 +0800

    feat: style modified

--- components/Layout/Container.js
+++ components/Layout/Container.js
@@ -1,3 +1,3 @@
 export default function Container({ children }) {
-  return <div className="container mx-auto px-5 flex justify-center flex-wrap">{children}</div>;
+  return <div className="container mx-auto px-5 flex justify-center md:flew-nowrap flex-wrap">{children}</div>;
 }
