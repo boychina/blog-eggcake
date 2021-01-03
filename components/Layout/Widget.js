134a3c4f3a1fb84951beae7e0c9123d174dc4573
boychina <boychina@outlook.com>
2021-01-03 23:19:11 +0800

    [WIP]feat: add word cloud widget

--- components/Layout/Widget.js
+++ components/Layout/Widget.js
@@ -1,5 +1,9 @@
+import dynamic from 'next/dynamic';
 import WidgetItem from '../Common/WidgetItem';
-import WordCloud from '../Common/WordCloud';
+// import WordCloud from '../Common/WordCloud';
+
+// const WordCloud = dynamic(() => import('../Common/WordCloud'));
+
 export default function Widget({ allPosts, keywords }) {
   return (
     <div className="col-span-12 md:col-span-2 lg:col-span-3 xl:col-span-4">
