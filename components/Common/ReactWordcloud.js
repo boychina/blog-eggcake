971cc7b316ddd775cf3e006b6c27df0bd7fdf398
boychina <boychina@outlook.com>
2021-01-07 00:46:31 +0800

    feat: wordcloud complete

--- components/Common/ReactWordcloud.js
+++ components/Common/ReactWordcloud.js
@@ -1,53 +1,55 @@
-import { createCanvas, loadImage } from 'canvas';
+import { createCanvas } from "canvas";
 import cloud from "d3-cloud";
-// var d3 = require("d3");
+import { random } from 'lodash';
+import { COLORS } from "@/config/constant";
+import { getKeywords } from "@/lib/api";
 
-const layout = cloud().size([300, 200])
-  .canvas(function () { return createCanvas(1, 1); })
+const layout = cloud()
+  .size([400, 300])
+  .canvas(() => createCanvas(400, 300))
   .padding(5)
-  .rotate(function () { return ~~(Math.random() * 2) * 90; })
+  .rotate(() => ~~(Math.random() * 2) * 90)
   .font("Impact")
-  .fontSize(function (d) { return d.size; });
+  .fontSize((d) => d.size);
 
-// const fill = d3.scale.category20();
+export default function ReactWordcloud({ keywords }) {
+  if (!keywords || !Object.keys(keywords).length) return null;
 
-const data = ["Hello", "world", "normally", "you", "want", "more", "words", "than", "this"]
-  .map(function (d) {
-    return { name: d, size: 10 + Math.random() * 90 };
-  });
-
-export default function ReactWordcloud() {
   let maxSize = 1;
 
-  let tags = data;
-  tags = tags.filter((tag) => {
-    return tag.size;
-  });
-
-  tags.forEach(function (tag) {
-    var size = tag.size;
-    if (size > maxSize)
-      maxSize = size;
+  Object.values(keywords).forEach(({ value }) => {
+    if (value > maxSize) {
+      maxSize = value;
+    };
   });
 
   //构建传入layout的words
-  let arr = [];
-  let words;
-  tags.forEach((tag) => {
-    arr.push({ name: tag.name, num: tag.size });
-  });
-  words = arr.map(function (d) {
-    const text = d.name.replace(/[^\x00-\xff]/g, "ab");//对中文的投机处理，用ab代替中文字符
-    return { name: d.name, text: text, size: Math.log(d.num) / (Math.log(maxSize) - Math.log(1)) * 15 + 30 };//size的计算取对数，是为了让标签之间的大小相对平均一些。因为博客侧重前端内容，所以某一些标签会比较多，标签最大最小次数的差距会比较大。
+  let words = [];
+  Object.keys(keywords).forEach((word) => {
+    const wordObj = keywords[word];
+    words.push({ ...wordObj, name: word, size: (Math.log(wordObj.value) / (Math.log(maxSize) - Math.log(1))) * 8 + 20 });
   });
   layout.words(words);
   layout.start();
 
   return (
     <div>
-      <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg" version="1.1">
-        <g transform="translate(300,200)">
-          {words.map((word, i) => <text text-anchor="middle" transform={`translate(${word.x}, ${word.y})rotate(${word.rotate})`} style={{ fontSize: word.size, fontFamily: 'Impact' }}>{word.name}</text>)}
+      <svg
+        width="400"
+        height="300"
+      >
+        <g transform="translate(200, 150)">
+          {words.map((word) => (
+            <text
+              key={word}
+              text-anchor="middle"
+              fill={COLORS[random(22)]}
+              transform={`translate(${word.x}, ${word.y})rotate(${word.rotate})`}
+              style={{ fontSize: word.size, fontFamily: "Impact" }}
+            >
+              {word.name}
+            </text>
+          ))}
         </g>
       </svg>
     </div>
--- components/Layout/Widget.js
+++ components/Layout/Widget.js
@@ -6,7 +6,7 @@ export default function Widget({ allPosts, keywords }) {
     <div className="col-span-12 md:col-span-2 lg:col-span-3 xl:col-span-4">
       <WidgetItem title="最近更新" data={allPosts.slice(0, 5)} />
       <WidgetItem title="更多" data={allPosts.slice(5)} />
-      <ReactWordcloud />
+      <ReactWordcloud keywords={keywords} />
     </div>
   );
 }
