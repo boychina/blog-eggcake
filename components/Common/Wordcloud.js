5571f297cea1291cd1bfb0369c4ef50daaabe8ea
boychina <boychina@outlook.com>
2021-01-07 23:38:17 +0800

    feat: tag search

--- components/Common/Wordcloud.js
+++ components/Common/Wordcloud.js
@@ -1,16 +1,15 @@
 import { createCanvas } from "canvas";
 import cloud from "d3-cloud";
-import { random } from 'lodash';
+import { random } from "lodash";
+import Link from "next/link";
 import { SendOutlined } from "@ant-design/icons";
 import { COLORS } from "@/config/constant";
-import { getKeywords } from "@/lib/api";
 
 const layout = cloud()
   .size([400, 300])
   .canvas(() => createCanvas(400, 300))
   .padding(2)
   .rotate(() => ~~(Math.random() * 2) * 90)
-  .font("Impact")
   .fontSize((d) => d.size);
 
 export default function ReactWordcloud({ title, tags }) {
@@ -21,14 +20,21 @@ export default function ReactWordcloud({ title, tags }) {
   Object.values(tags).forEach(({ value }) => {
     if (value > maxSize) {
       maxSize = value;
-    };
+    }
   });
 
   //构建传入layout的words
   let words = [];
   Object.keys(tags).forEach((word) => {
     const wordObj = tags[word];
-    words.push({ ...wordObj, name: word, size: (Math.log(wordObj.value) * 4 / (Math.log(maxSize) - Math.log(1))) * 4 + 20 });
+    words.push({
+      ...wordObj,
+      name: word,
+      size:
+        ((Math.log(wordObj.value) * 4) / (Math.log(maxSize) - Math.log(1))) *
+          4 +
+        20,
+    });
   });
   layout.words(words);
   layout.start();
@@ -39,20 +45,19 @@ export default function ReactWordcloud({ title, tags }) {
         <SendOutlined />
         <span className="ml-1">{title}</span>
       </div>
-      <svg
-        width="400"
-        height="300"
-        >
-        <g transform="translate(180, 150)">
+      <svg width="400" height="300">
+        <g transform="translate(190, 150)">
           {words.map((word) => (
             <text
               key={word.name}
               text-anchor="middle"
               fill={COLORS[random(10)]}
               transform={`translate(${word.x}, ${word.y})rotate(${word.rotate})`}
-              style={{ fontSize: word.size, fontFamily: "Impact" }}
-              >
-              {word.name}
+              style={{ fontSize: word.size }}
+            >
+              <Link as={`/search/${word.name}`} href="/search/[tag]">
+                <a>{word.name}</a>
+              </Link>
             </text>
           ))}
         </g>
---
+++ pages/search/[tag].js
@@ -0,0 +1,57 @@
+import { useRouter } from "next/router";
+import { getAllPosts, getTags, getPostsByTag } from "@/lib/api";
+import Content from "@/components/Home/Content";
+
+export default function Post({ allPosts, postsByTag, tags }) {
+  const router = useRouter();
+  return (
+    <Content
+      allPosts={allPosts}
+      postsByPageIndex={postsByTag}
+      current={1}
+      totalPage={1}
+      tags={tags}
+    />
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const allPosts = getAllPosts([
+    "title",
+    "date",
+    "slug",
+    "author",
+  ]);
+  const postsByTag = getPostsByTag(decodeURIComponent(params.tag), [
+    "title",
+    "date",
+    "tag",
+    "slug",
+    "author",
+    "coverImage",
+    "excerpt",
+  ]);
+  const tags = getTags();
+  return {
+    props: {
+      allPosts,
+      postsByTag,
+      tags,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const tags = getTags();
+  const result = {
+    paths: Object.keys(tags).map((tag) => {
+      return {
+        params: {
+          tag: encodeURIComponent(tag),
+        },
+      };
+    }),
+    fallback: false,
+  };
+  return result;
+}
