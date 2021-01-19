98379742a8fb44ec66ee2fcdc64386e64e501706
boychina <boychina@outlook.com>
2021-01-19 23:37:38 +0800

    feat: blog calendar

---
+++ components/Common/Calendars.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { Calendar } from "antd";
+import { SendOutlined } from "@ant-design/icons";
+
+export default function Calendars({ title }) {
+  const onPanelChange = (value, mode) => {
+    console.log(value, mode);
+  };
+  return (
+    <div>
+      <div className="flex items-center" style={{ padding: "12px 0" }}>
+        <SendOutlined />
+        <span className="ml-1">{title}</span>
+      </div>
+      <div
+        style={{ width: 360, border: "1px solid #f0f0f0", borderRadius: "2px" }}
+      >
+        <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+      </div>
+    </div>
+  );
+}
--- components/Layout/Widget.js
+++ components/Layout/Widget.js
@@ -1,9 +1,11 @@
-import WordCloud from '../Common/WordCloud';;
+import Calendars from '../Common/Calendars';
+import WordCloud from '../Common/WordCloud';
 import WidgetItem from '../Common/WidgetItem';
 
 export default function Widget({ allPosts, tags }) {
   return (
     <div className="col-span-12 md:col-span-2 lg:col-span-3 xl:col-span-4">
+      <Calendars title="博客日历" />
       <WordCloud title="标签" tags={tags} />
       <WidgetItem title="最近更新" data={allPosts.slice(0, 5)} />
       <WidgetItem title="更多" data={allPosts.slice(5)} />
