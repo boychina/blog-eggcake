347ba3a337fb5b4f7714a6eb4e50d06841ae3f1f
boychina <boychina@outlook.com>
2021-02-05 14:57:44 +0800

    feat: prevBtn,nextBtn style modified

--- components/Post/PrevNextBtns.js
+++ components/Post/PrevNextBtns.js
@@ -1,29 +1,30 @@
 import Link from "next/link";
 import { ArrowLeftOutlined, ArrowRightOutlined } from '@ant-design/icons';
+import { Row, Col } from 'antd';
 
 export default function PrevNextBtns({ prevNextPost }) {
   const { prevPost, nextPost } = prevNextPost;
 
   return (
-    <section className="flex justify-between flex-wrap my-8">
-      <div style={{ marginTop: 2 }}>
+    <Row className="flex justify-between my-8" gutter={12}>
+      <Col span={12}>
         {nextPost && 
           <Link as={`/posts/${nextPost.slug}`} href="/posts/[slug]">
-            <div className="whitespace-nowrap cursor-pointer flex items-center h-10 text-sm bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-2 px-4 duration-200 transition-colors">
+            <div className="whitespace-nowrap cursor-pointer flex items-center h-full text-sm bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-2 px-4 duration-200 transition-colors">
               <ArrowLeftOutlined />
               <span className="ml-2">{nextPost.title}</span>
             </div>
           </Link>}
-      </div>
-      <div style={{ marginTop: 2 }}>
+      </Col>
+      <Col span={12}>
         {prevPost && 
           <Link as={`/posts/${prevPost.slug}`} href="/posts/[slug]">
-            <div className="whitespace-nowrap cursor-pointer flex items-center h-10 text-sm bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-2 px-4 duration-200 transition-colors">
+            <div className="whitespace-nowrap cursor-pointer flex items-center h-full text-sm bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-2 px-4 duration-200 transition-colors">
               <span className="mr-2">{prevPost.title}</span>
               <ArrowRightOutlined />
             </div>
           </Link>}
-      </div>
-    </section>
+      </Col>
+    </Row>
   )
 }
