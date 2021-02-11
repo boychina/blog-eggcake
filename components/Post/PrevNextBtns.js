0c88a47f65ff150fdba5c137ce33f4b2a7bdc6e8
boychina <boychina@outlook.com>
2021-02-11 15:12:30 +0800

    feat: prev next button modiefied

--- components/Post/PrevNextBtns.js
+++ components/Post/PrevNextBtns.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import { ArrowLeftOutlined, ArrowRightOutlined } from '@ant-design/icons';
-import { Row, Col } from 'antd';
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@ant-design/icons";
+import { Row, Col } from "antd";
 
 export default function PrevNextBtns({ prevNextPost }) {
   const { prevPost, nextPost } = prevNextPost;
@@ -8,23 +8,25 @@ export default function PrevNextBtns({ prevNextPost }) {
   return (
     <Row className="flex justify-between my-8" gutter={12}>
       <Col span={12}>
-        {nextPost && 
-          <Link as={`/posts/${nextPost.slug}`} href="/posts/[slug]">
+        {prevPost && (
+          <Link as={`/posts/${prevPost.slug}`} href="/posts/[slug]">
             <div className="whitespace-nowrap cursor-pointer flex items-center h-full text-sm bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-2 px-4 duration-200 transition-colors">
               <ArrowLeftOutlined />
-              <span className="ml-2">{nextPost.title}</span>
+              <span className="mr-2">{prevPost.title}</span>
             </div>
-          </Link>}
+          </Link>
+        )}
       </Col>
       <Col span={12}>
-        {prevPost && 
-          <Link as={`/posts/${prevPost.slug}`} href="/posts/[slug]">
-            <div className="whitespace-nowrap cursor-pointer flex items-center justify-between h-full text-sm bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-2 px-4 duration-200 transition-colors">
-              <span className="mr-2">{prevPost.title}</span>
+        {nextPost && (
+          <Link as={`/posts/${nextPost.slug}`} href="/posts/[slug]">
+            <div className="whitespace-nowrap cursor-pointer flex items-center h-full text-sm bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-2 px-4 duration-200 transition-colors justify-between">
+              <span className="ml-2">{nextPost.title}</span>
               <ArrowRightOutlined />
             </div>
-          </Link>}
+          </Link>
+        )}
       </Col>
     </Row>
-  )
+  );
 }
